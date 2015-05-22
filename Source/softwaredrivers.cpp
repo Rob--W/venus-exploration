3cc0502d55c03f0277e95bbcb1fa96cc979a2cfb
TimComm <tmcmmndr@gmail.com>
2015-05-22 09:59:00 +0200

    Layout and comments in softwaredrivers
    Unified the bracket styles and added comments to show marijn where he
    can add scanning functions which need to be executed during the drive.

--- Source/softwaredrivers.cpp
+++ Source/softwaredrivers.cpp
@@ -22,7 +22,8 @@ const int pingPin = 9;
 
 
 
-void startSetup(){
+void startSetup()
+{
 	venusLeft.attach(leftservo, 540, 2400);
 	venusRight.attach(rightservo, 540, 2400);
 	servoUltra.attach(ultraservo, 540, 2400);
@@ -32,25 +33,30 @@ void startSetup(){
 	pinMode(rightencoder, INPUT);
 }
 
-void turn(){
+void turn()
+{
 	venusLeft.write(120);
 	venusRight.write(120);
 }
 
-int readIRLB(){		//Reads IR Left Bottom. Return unit to be determined
+int readIRLB() //Reads IR Left Bottom. Return unit to be determined
+{
 
 }
 
-int readIRRB(){		//Reads IR Right Bottom. Return unit to be determined
+int readIRRB() //Reads IR Right Bottom. Return unit to be determined
+{
 
 }
 
-void stop(){	//Stop function, makes the robot stop driving (if the servo's are properly set)
+void stop() //Stop function, makes the robot stop driving (if the servo's are properly set)
+{
 	venusLeft.write(90);
 	venusRight.write(90);
 }
 
-void servoDrive(int left, int right, int n){ 
+void servoDrive(int left, int right, int n)
+{ 
 	int pulse;
 	int prevpulse;
 	int countpulse = 0;
@@ -60,7 +66,13 @@ void servoDrive(int left, int right, int n){
 	venusLeft.write(left);
 	venusRight.write(right);
 
-	while (n>countpulse){
+	//////////////////////////////////////////////////////////////////////////
+	//																		//
+	//   Continuous while loop to place scanning function in for Marijn		//
+	//		vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv			//
+	//////////////////////////////////////////////////////////////////////////
+	while (n>countpulse)
+	{
 		pulse = digitalRead(rightencoder);
 		if (pulse != prevpulse){
 			prevpulse = pulse;
@@ -69,23 +81,27 @@ void servoDrive(int left, int right, int n){
 		//400 is arbitrary between  0 and 1024. This has to be tested. 
 		//The value is somewhere in between 0 and 2^8 because of a change in light and dark reception of the sensor.
 		//it will never be completly dark, that is why the value will never be zero.
-		/*if (readIRLB() < 400 || readIRRB() < 400){//hole sensing code has to be added
+		/*if (readIRLB() < 400 || readIRRB() < 400)  //hole sensing code has to be added
+		{
 			break; //Some kind of notification has to be added that an emergency brake has been executed.
 		}*/
 	}
 	stop();
 }
 
-void drive(int distance, int angle){ //drive function with integer parameters distance and angle in degrees
+void drive(int distance, int angle)  //drive function with integer parameters distance and angle in degrees
+{
 	//turn part
 	/*angle = angle % 360;*/ //2*pirad*k=2*pirad*(k+1)
-	if (angle > 0){	//turn left
+	if (angle > 0)  //turn left
+	{	
 		int turnn = (((angle)* 100) / 1437); //Number of pulses needed to turn. turns with both wheels
 		servoDrive(100, 100, turnn);
 		//int turnn = ((angle * 200) / 1437); //turns with just one wheel
 		//servoDrive(90, 110, turnn);
 	}
-	else{	//turn right
+	else   //turn right
+	{	
 		int turnn = (((angle) *100) / 1437); //Number of pulses needed to turn. turns with both wheels
 		servoDrive(80, 80, turnn);
 		//int turnn = (((360-angle) * 200) / 1437); //turns with just one wheel
@@ -96,21 +112,25 @@ void drive(int distance, int angle){ //drive function with integer parameters di
 	servoDrive(180, 0, n);
 }
 
-void reverse(int distance){	//Makes the robot drive backwards a certain distance
+void reverse(int distance) //Makes the robot drive backwards a certain distance
+{	
 	int n = ((distance*100) / 1317); //Number of pulses needed to drive distance
 	Serial.println(n);
 	servoDrive(0, 180, n);
 }
 
-int readIRMid(){	//reads Middle IR. Return unit to be determined
+int readIRMid() //reads Middle IR. Return unit to be determined
+{
 
 }
 
-int readIRGrab(){		//Reads IR Grabber. Return unit to be determined
+int readIRGrab()    //Reads IR Grabber. Return unit to be determined
+{		
 
 }
 
-int readLightSense(){	//Reads Light Sensor. Return unit to be determined
+int readLightSense()   //Reads Light Sensor. Return unit to be determined
+{	
 
 }
 
@@ -122,7 +142,8 @@ int microsecondsToCentimeters(int microseconds)
 	return microseconds / 29 / 2;
 }
 
-int readUltraTop(int angle){		//Reads Top Ultrasonic sensor. Return unit is a distance. Input parameter is a angle; full left is angle 0 degrees full right is 180 degrees
+int readUltraTop(int angle)  //Reads Top Ultrasonic sensor. Return unit is a distance. Input parameter is a angle; full left is angle 0 degrees full right is 180 degrees
+{		
 	servoUltra.write(angle);
 	// establish variables for duration of the ping, 
 	// and the distance result in inches and centimeters:
@@ -156,7 +177,8 @@ int readUltraTop(int angle){		//Reads Top Ultrasonic sensor. Return unit is a di
 	return cm;
 }
 
-int readUltraBot(){
+int readUltraBot()
+{
 	// establish variables for duration of the ping, 
 	// and the distance result in inches and centimeters:
 	int duration, inches, cm;
@@ -188,12 +210,14 @@ int readUltraBot(){
 	delay(100);
 	return cm;
 }
-void openGrabber(){
+void openGrabber()
+{
 	//servoGrabber.attach(grabberservo);
 	servoGrabber.write(0);
 	//servoGrabber.detach();
 }
-void closeGrabber(){
+void closeGrabber()
+{
 	// servoGrabber.attach(grabberservo);
 	servoGrabber.write(180);
 	//servoGrabber.detach();

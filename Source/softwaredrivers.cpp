6ea22403ba31d16a245b00992f815ddae675fbc5
TimComm <tmcmmndr@gmail.com>
2015-05-22 09:38:52 +0200

    Again possible fix for drive();
    Changed the turn left function to exactly the same as the turn right
    function.

--- Source/softwaredrivers.cpp
+++ Source/softwaredrivers.cpp
@@ -79,8 +79,8 @@ void servoDrive(int left, int right, int n){
 void drive(int distance, int angle){ //drive function with integer parameters distance and angle in degrees
 	//turn part
 	/*angle = angle % 360;*/ //2*pirad*k=2*pirad*(k+1)
-	if (angle >= 0){	//turn left
-		int turnn = round((angle *100.0) / 1437.0); //Number of pulses needed to turn. turns with both wheels
+	if (angle > 0){	//turn left
+		int turnn = (((angle)* 100) / 1437); //Number of pulses needed to turn. turns with both wheels
 		servoDrive(100, 100, turnn);
 		//int turnn = ((angle * 200) / 1437); //turns with just one wheel
 		//servoDrive(90, 110, turnn);
